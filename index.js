56f842bf80174ee401ecbb20b804900c68f9223f
Christianrslv <christianrslv22@gmail.com>
2022-09-26 15:11:56 -0300

    Test concat style

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ const Diff2html = require('diff2html');
 const Handlebars = require('handlebars');
 const fs = require('fs').promises;
 
+  
+const jsscript = '<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/diff2html/bundles/js/diff2html-ui.min.js"></script>';
+const css = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.1/styles/github.min.css" />
+<link
+  rel="stylesheet"
+  type="text/css"
+  href="https://cdn.jsdelivr.net/npm/diff2html/bundles/css/diff2html.min.css"
+/>`;
+
 try {
   const diff = getInput('git-diff');
-  const diff2html = generateDiff2Html(diff);
-  const contentTemplateFile = await fs.readFile(HBS_TEMPLATE, 'utf8');
-  const template = Handlebars.compile(contentTemplateFile);
-  const hbsresul = template({diff2html: diff2html});
+  const diff2html = generateDiff2Html();
+  // const contentTemplateFile = await fs.readFile(HBS_TEMPLATE, 'utf8');
+  // const template = Handlebars.compile(contentTemplateFile);
+  // const hbsresul = template({diff2html: diff2html});
+  const hbsresul = `${jsscript}
+  ${css}
+  ${diff2html}`;
+  console.log(hbsresul);
   setOutput("diff2html", hbsresul);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2)
